7a7dfd224c0b020974609014911d207a4532865e
Max Chesnikov <48633192+maxches99@users.noreply.github.com>
2022-12-07 19:07:51 +0300

    Initial Commit

---
+++ AppleWatchGame Watch App/AppleWatchGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleWatchGameApp.swift
+//  AppleWatchGame Watch App
+//
+//  Created by Максим Чесников on 07.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleWatchGame_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
